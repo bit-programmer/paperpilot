9f20becc8319b399ce16352396aabc50d5ecdc6b
bit-programmer <harsh.81928@gmail.com>
2026-06-11 01:30:48 +0530

    :tada: Added the env checker

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -8,6 +8,10 @@ const stringBoolean = z.coerce.string().transform((val) => {
 
 const EnvSchema = z.object({
     NODE_ENV: z.string().default("development"),
+    LOG_LEVEL: z.string().default("debug"),
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string(),
+    APP_NAME: z.string(),
     DB_HOST: z.string(),
     DB_USER: z.string(),
     DB_PASSWORD: z.string(),
@@ -15,7 +19,12 @@ const EnvSchema = z.object({
     DB_PORT: z.coerce.number(),
     DATABASE_URL: z.string(),
     DB_MIGRATING: stringBoolean,
-    DB_SEEDING: stringBoolean
+    DB_SEEDING: stringBoolean,
+    MAIL_HOST: z.string(),
+    MAIL_PORT: z.coerce.number(),
+    MAIL_USERNAME: z.string(),
+    MAIL_PASSWORD: z.string(),
+    MAIL_ID: z.string()
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
