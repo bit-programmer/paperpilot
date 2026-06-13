c4bb7914242518c575148d7f1a6b7db160aba002
bit-programmer <harsh.81928@gmail.com>
2026-06-13 21:00:44 +0530

    ✏️ Added pages

---
+++ app/ui/confirm-email-notification/page.tsx
@@ -0,0 +1,7 @@
+const ConfirmPasswordNotification = () => {
+    return (
+        <div>Please navigate to your mail to confirm your mail address</div>
+    );
+}
+
+export default ConfirmPasswordNotification;
